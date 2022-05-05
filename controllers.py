39a02be11555bb057a392fe5989b790c7b086b93
Ryan Millett <96157608+RyanMillett@users.noreply.github.com>
2022-05-05 15:23:16 -0700

    Update controllers.py
    Stubbed out functions.  Definitions are still placeholder code.

--- controllers.py
+++ controllers.py
@@ -25,9 +25,19 @@ session, db, T, auth, and tempates are examples of Fixtures.
 Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
 """
 
-from py4web import action, request, abort, redirect, URL
+import uuid
+
+from py4web import action, request, abort, redirect, URL, Field
+from py4web.utils.form import Form, FormStyleBulma
+from py4web.utils.url_signer import URLSigner
+
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
+
+from .models import get_user_email
+from pydal.validators import *
+
+url_signer = URLSigner(session)
 
 
 @action("index")
@@ -37,3 +47,47 @@ def index():
     message = T("Hello {first_name}".format(**user) if user else "Hello")
     actions = {"allowed_actions": auth.param.allowed_actions}
     return dict(message=message, actions=actions)
+
+# Add Prompt
+@action('add', method=["GET", "POST"])
+@action.uses(db, session, auth.user, 'add.html')
+def add():
+    form = # ADD TO DB
+    if(form.accepted):
+        redirect(URL('index'))
+
+    return dict(form=form)
+
+# Edit Prompt
+@action('edit/<contact_id:int>', method=["GET", "POST"])
+@action.uses(db, session, auth.user, url_signer.verify(), 'edit.html')
+def edit(contact_id = None):
+    assert contact_id is not None
+    p = # TODO
+    if p is None:
+        redirect(URL('index'))
+
+    form = # TODO
+    if(form.accepted):
+        redirect(URL('index'))
+
+    return dict(form=form)
+
+# Delete Prompt
+@action('delete/<contact_id:int>', method=["GET", "POST"])
+@action.uses(db, session, auth.user, url_signer.verify())
+def delete(contact_id=None):
+    assert contact_id is not None
+    db(db.contact.id == contact_id).delete()
+    redirect(URL('index'))
+
+# Vote Up / Down
+@action('inc/<bird_id:int>') # the :int means: please convert this to an int.
+@action.uses(db, session, auth.user, url_signer.verify())
+# ... has to match the bird_id parameter of the Python function here.
+def vote(bird_id=None):
+    # TODO
+    assert bird_id is not None
+    bird = db.bird[bird_id]
+    db(db.bird.id == bird_id).update(n_sightings=bird.n_sightings+1)
+    redirect(URL('index'))
